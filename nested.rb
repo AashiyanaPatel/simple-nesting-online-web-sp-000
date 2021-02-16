5aa94782258edcb96e85cc894016b7e850a85b46
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-16 21:40:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,6 +59,8 @@ end
 
 def adding_matz
   programmer_hash:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash
+end
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
